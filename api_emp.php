7fdd25fc22b4dcb4045816fcf985466f20d3efb9
Aaron Armborst <824033@uni-wuppertal.de>
2024-05-02 18:17:14 +0200

    x

---
+++ api_emp.php
@@ -0,0 +1,16 @@
+<?php
+	
+include('config.php');
+
+$json = file_get_contents('https://'.$location.'.emp-access.de/api_gate.php?hardware='.$hardware.'&id='.$scan.'');
+$json = json_decode($json, true);
+		
+		if($json[access] == 1)
+		{
+			$access = 1;
+		}else
+		{
+			$access = 0;
+		}
+		
+?>
--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 	$location = "twincable";
-
-	$name = "Test Pi - Eingang";
+	$hardware = 1;
+	$name = "Strandbad Eingang 1 - Einlass";
 	
 	//Eingang:1, Ausgang: 2, Locker:3 
 	$typ = "1";
